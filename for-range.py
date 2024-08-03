a2a84325d586668d3ef2aab01d7fb3379845832e
Raghuraj Singh <hannuseven@gmail.com>
2024-08-03 10:14:39 +0530

    range function

---
+++ for-range.py
@@ -0,0 +1,19 @@
+for number in range(1, 10):
+    print(number)
+
+print("\n")
+
+for odd in range(1,10,2):
+    print(odd) 
+ 
+print("\n")
+
+total = 0
+for i in range(1, 101):
+    total += i
+print(total)  
+
+
+
+
+
